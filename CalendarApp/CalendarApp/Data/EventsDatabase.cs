2b8012764c809cffae249fd763e741fc88b01851
Shubham JIndal <shubhamjindal1234@gmail.com>
2019-09-23 12:55:12 +1000

    Add model and database for events.

---
+++ CalendarApp/CalendarApp/Data/EventsDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CalendarApp.Model;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MarcTron.Plugin;
+
+namespace CalendarApp.Data
+{
+    public class EventsDatabase
+    {
+        public SQLiteAsyncConnection Events_database { get; private set; }
+        public EventsDatabase()
+        {
+            Events_database = MtSql.Current.GetConnectionAsync("eventsDb");
+
+            Events_database.CreateTableAsync<EventsModel>().Wait();
+
+            Events_database.InsertAsync(new EventsModel { EventTitle = "Event 1", EventDescription = "Description 1", EventDate = DateTime.Now });
+            Events_database.InsertAsync(new EventsModel { EventTitle = "Event 2", EventDescription = "Description 2", EventDate = DateTime.Now });
+            Events_database.InsertAsync(new EventsModel { EventTitle = "Event 3", EventDescription = "Description 3", EventDate = DateTime.Now });
+        }
+
+        public async Task<List<NotesModel>> GetAllNotesAsync()
+        {
+            return await Events_database.Table<NotesModel>().ToListAsync();
+        }
+
+        public async Task AddNotesAsync(NotesModel newNote)
+        {
+            await Events_database.InsertAsync(newNote);
+        }
+    }
+}
---
+++ CalendarApp/CalendarApp/Model/EventsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SQLite;
+
+namespace CalendarApp.Model
+{
+    public class EventsModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string EventTitle { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime EventDate { get; set; }
+        public EventsModel(string title, string desc, DateTime date)
+        {
+            EventTitle = title;
+            EventDescription = desc;
+            EventDate = date;
+        }
+        public EventsModel()
+        {
+        }
+    }
+}
